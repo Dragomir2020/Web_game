27fdfe008cbd79f3e3217c5270d0622f5a2c0359
dragomdv <dragomdv@mail.uc.edu>
2017-11-10 17:51:48 -0500

    Change aspect ratio and add music

---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	//Must be static so only one exists
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if (instance != null) {
+			//If music player already exists destroy gameObject
+			Destroy (gameObject);
+		} else {
+			//Make sure music player persists between scenes
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
